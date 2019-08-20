ca7cbcf11e0c1539ade2d71fb2ca0f31b398428f
kassyinkyoto11 <kassyinkyoto11@gmail.com>
2019-08-20 12:40:11 +0900

    finish redirect after signin signout signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+   
+   #以下を追記
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name,:profile])
+    end
+
+    #ログアウト後
+    def after_sign_out_path_for(resource)
+        root_path
+    end
+ 
+    #ログイン後
+    def after_sign_in_path_for(resource)
+        mypage_path(resource)
+    end
+
+    #新規登録後
+    def after_sign_up_path_for(resource)
+        mypage_path(resource)
+    end
 end
+
